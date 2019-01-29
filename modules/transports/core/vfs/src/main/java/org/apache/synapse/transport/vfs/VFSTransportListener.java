c50d03fc390366db13240da2677f674a2d055e3b
TharinduWijewardane <tharinduw@wso2.com>
2019-01-29 10:01:34 +0530

    Fix VFS transport issues
    Add capability to specify url parameters for MoveAfterProcess
    and MoveAfterFailure parameters in vfs proxy.
    Fixes wso2/product-ei#3088
    
    Set sftp file options regardless of FileURI scheme since the
    sftp options are required when one of FileURI,MoveAfterProcess
    or ActionAfterFailure is set to use sftp it is incorrect not to set
    sftp file options based only on the scheme of FileURI.
    Fixes wso2/product-ei#3035
    
    Decrypt parameters if the encrypted content is specified in the
    {wso2:vault-decrypt('param value')} format. Applied to all the
    parameters specified in a vfs proxy. Since the decrypted parameter
    values are kept in the ParameterInclude instance it self they can be
    used later without having to be decrypted again.
    Fixes wso2/product-ei#3125, Fixes wso2/product-ei#3031

--- modules/transports/core/vfs/src/main/java/org/apache/synapse/transport/vfs/VFSTransportListener.java
+++ modules/transports/core/vfs/src/main/java/org/apache/synapse/transport/vfs/VFSTransportListener.java
@@ -537,8 +537,8 @@ public class VFSTransportListener extends AbstractPollingTransportListener<PollT
                                 		+ "process");
                             }
                         }
-
                         close(child);
+
                         if(iFileProcessingInterval != null && iFileProcessingInterval > 0){
                         	try{
                                 if (log.isDebugEnabled()) {
@@ -673,7 +673,15 @@ public class VFSTransportListener extends AbstractPollingTransportListener<PollT
             }
 
             if (moveToDirectoryURI != null) {
-                FileObject moveToDirectory = fsManager.resolveFile(moveToDirectoryURI, fso);
+                // This handles when file needs to move to a different file-system
+                FileSystemOptions destinationFSO = null;
+                try {
+                    destinationFSO = VFSUtils.attachFileSystemOptions(
+                            VFSUtils.parseSchemeFileOptions(moveToDirectoryURI, entry.getParams()), fsManager);
+                } catch (Exception e) {
+                    log.warn("Unable to set options for processed file location ", e);
+                }
+                FileObject moveToDirectory = fsManager.resolveFile(moveToDirectoryURI, destinationFSO);
                 String prefix;
                 if(entry.getMoveTimestampFormat() != null) {
                     prefix = entry.getMoveTimestampFormat().format(new Date());
