f2faa6b4e26f3a17317450fab138d6f4678845a3
malakaganga <malakaganga.ac@gmail.com>
2019-07-22 15:21:56 +0530

    Fix wso2/product-ei/issues/4082
    Check setCharacterEncoding property prior to append charset
    encoding to outgoing request
    
    Fixes: https://github.com/wso2/product-ei/issues/4082

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughHttpSender.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughHttpSender.java
@@ -600,8 +600,8 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
                             contentTypeValueInMsgCtx.contains(PassThroughConstants.CONTENT_TYPE_MULTIPART_FORM_DATA))) {
 
                        // adding charset only if charset is not available,
-                       if (contentTypeValueInMsgCtx.indexOf(HTTPConstants.CHAR_SET_ENCODING) == -1
-                           && format != null) {
+                       if (format != null && contentTypeValueInMsgCtx.indexOf(HTTPConstants.CHAR_SET_ENCODING) == -1 &&
+                               !"false".equals(msgContext.getProperty(PassThroughConstants.SET_CHARACTER_ENCODING))) {
 							String encoding = format.getCharSetEncoding();
 							if (encoding != null) {
 								sourceResponse.removeHeader(HTTP.CONTENT_TYPE);
